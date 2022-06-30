bbc63abf14374a5ff1d574f1efbc3ad558cc622c
Hyeon <ahyeon1117@dtonic.io>
2022-06-30 16:23:14 +0900

    spring init

---
+++ backend/src/test/java/com/mnb/contest/metabus/MetabusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnb.contest.metabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetabusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
